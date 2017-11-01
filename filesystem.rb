a61febbd7957e3d80e10a187c89ed0b42077568e
thingsym <thingsym@gmail.com>
2017-11-01 21:08:46 +0900

    add path

--- filesystem.rb
+++ filesystem.rb
@@ -1,7 +1,9 @@
 acl {
   deny { path =~ /readme\.[html|txt]/ }
+  deny { path =~ /license\.txt/ }
   deny { path =~ /\.htaccess/ }
   deny { path =~ /wp\-config\.php/ }
+  deny { path =~ /wp\-config\-sample\.php/ }
   deny { path =~ /wp\-includes\/.*\.php/ }
   deny { path =~ /wp\-admin\/includes\/.*\.php/ }
   deny { path =~ /wp\-content\/.*\.php/ }
