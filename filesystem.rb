a9eda1931c2ef8fc6c4d51f65fd7126cde137653
thingsym <thingsym@gmail.com>
2017-11-23 20:13:34 +0900

    add path

--- filesystem.rb
+++ filesystem.rb
@@ -5,6 +5,10 @@ acl {
   deny { path =~ /wp\-config\.php/ }
   deny { path =~ /wp\-config\-sample\.php/ }
   deny { path =~ /wp\-includes\/.*\.php/ }
+  deny { path =~ /wp\-includes\/js\/tinymce\/langs\/.*\.php/ }
+  deny { path =~ /wp\-includes\/theme\-compat\// }
   deny { path =~ /wp\-admin\/includes\/.*\.php/ }
   deny { path =~ /wp\-content\/.*\.php/ }
+  # deny { path =~ /wp\-admin\/install\.php/ }
+  # deny { path =~ /xmlrpc\.php/ }
 }
