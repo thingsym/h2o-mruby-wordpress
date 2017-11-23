dc7868dde0c194d54c6323696abcada528055b03
thingsym <thingsym@gmail.com>
2017-11-23 20:21:38 +0900

    add extensions

--- cache-control.rb
+++ cache-control.rb
@@ -1,6 +1,6 @@
 acl {
   # Expires 7 day
   respond(399, {"cache-control" => "max-age=604800"}, []) {
-    path =~ /\.(css|js|jpg|jpeg|png|gif|svg)\z/
+    path =~ /\.(css|js|jpg|jpeg|png|gif|ico|svg|eot|ttf|otf|woff)\z/
   }
 }
