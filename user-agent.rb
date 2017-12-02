d74b554de2384951a67e3e852245677fb9288dae
thingsym <thingsym@gmail.com>
2017-12-02 23:27:35 +0900

    add bots to user_agent

--- user-agent.rb
+++ user-agent.rb
@@ -1,3 +1,18 @@
 acl {
   # deny { user_agent.match(/Chrome/i) }
+
+  # bots
+  # deny { user_agent.match(/Applebot/i) }
+  # deny { user_agent.match(/baiduspider/i) }
+  # deny { user_agent.match(/Bingbot/i) }
+  # deny { user_agent.match(/Googlebot/i) }
+  # deny { user_agent.match(/ia_archiver/i) }
+  # deny { user_agent.match(/msnbot/i) }
+  # deny { user_agent.match(/Naverbot/i) }
+  # deny { user_agent.match(/seznambot/i) }
+  # deny { user_agent.match(/Slurp/i) }
+  # deny { user_agent.match(/teoma/i) }
+  # deny { user_agent.match(/Twitterbot/i) }
+  # deny { user_agent.match(/Yandex/i) }
+  # deny { user_agent.match(/Yeti/i) }
 }
