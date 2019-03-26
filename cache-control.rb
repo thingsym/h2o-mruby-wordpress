64315656acc31f90492bcbe9c6e17e2bb3e516ec
thingsym <thingsym@gmail.com>
2019-03-26 20:16:39 +0900

    perf: add filename extensions

--- cache-control.rb
+++ cache-control.rb
@@ -1,6 +1,6 @@
 acl {
   # Expires 7 days
   respond(399, {"cache-control" => "max-age=604800"}, []) {
-    path =~ /\.(css|js|jpg|jpeg|png|gif|ico|svg|eot|ttf|otf|woff)\z/
+    path =~ /\.(css|js|jpg|jpeg|png|gif|wbmp|webp|ico|svg|svgz|eot|ttf|otf|woff)\z/
   }
 }
