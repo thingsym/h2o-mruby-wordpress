05f74f1fdef9342ee3a41b079edaafa488eda518
thingsym <thingsym@gmail.com>
2019-04-15 00:17:58 +0900

    fix: allow only specific domain

--- wp-admin.rb
+++ wp-admin.rb
@@ -4,7 +4,11 @@ acl {
   # Whitelist IP Addresses
   # allow { addr.start_with?("192.168.82.1") && path.start_with?("/wp-login.php") }
   # allow { addr.start_with?("192.168.82.1") && path.start_with?("/wp-admin") }
- 
+
+  # Specific domain only
+  # allow { header('HOST').match(/example\.com/i) && path.start_with?("/wp-login.php") }
+  # allow { header('HOST').match(/example\.com/i) && path.start_with?("/wp-admin") }
+
   # Basic Authentication
   # require "htpasswd.rb"
   # use( Htpasswd.new("/path/to/h2o-mruby-wordpress/.htpasswd", "realm") ) { path.start_with?("/wp-login.php") }
